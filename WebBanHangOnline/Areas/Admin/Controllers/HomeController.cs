2d98e968b093fc229c56bed221d64cf4d4cf104f
ThanhTC266 <93926196+ThanhTC266@users.noreply.github.com>
2023-02-10 18:30:42 +0700

    20230210_ThanhTC#Lesson8continue

--- WebBanHangOnline/Areas/Admin/Controllers/HomeController.cs
+++ WebBanHangOnline/Areas/Admin/Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace WebBanHangOnline.Areas.Admin.Controllers
         {
             return View();
         }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
     }
 }
